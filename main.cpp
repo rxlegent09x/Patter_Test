1923482f1dd7280ea1fdc626a73880c420bbc892
{LEGENT_09X} <115865060+rxlegent09x@users.noreply.github.com>
2025-11-08 15:49:02 +0530

    Refactor main.cpp to improve array initialization
    new assumption to do the same with a new style

--- main.cpp
+++ main.cpp
@@ -1,36 +1,34 @@
 #include<iostream>
 using namespace std;
+
+void fill_with_Value(int *x, int val, int size){
+    for(int i=0; i<size; i++){
+        x[i]=val;
+    }
+}
+
 int main(){
 
 //can be edit the arr values to change the pattern
 
-    int arr[][2] = {{0,0},{0,1},{0,2},{0,3},{0,4},{0,5},{0,6},{0,7},{0,8},{0,9},{1,4},{2,4},{1,5},{2,5},{1,0},{1,1},{1,2},{1,3},{1,6},{1,7},{1,8},{1,9},{3,4},{3,5},{4,4},{4,5},{4,3},{4,2},{4,1},{4,0},{5,0},{5,1},{5,2},{5,3},{5,4},{5,5},{6,1},{6,2},{7,2},{7,3},{8,3},{8,4},{9,4},{9,5}};
+    int arr[][2] = {{0,5},{0,4},{0,3},{0,2},{0,1},{0,0},{1,0},{2,0},{3,0},{3,1},{3,2},{3,3},{4,0},{5,0},{6,0},{7,0},{8,0},{6,2},{7,2},{8,2},{8,3},{8,4},{7,4},{6,4},{5,6},{6,6},{7,6},{8,6},{6,7},{5,8},{4,9},{7,8},{8,9}};
 
 int size_arr = (sizeof(arr)/sizeof(int))/2;
+int grid_size = 100;
+// cout<<size_arr<<endl;
 
-//cout<<size_arr<<endl;
-int temp=0;
-int i_j = 10;
+int x_arr[grid_size];
 
-for(int i=0; i<i_j; i++){
-    for(int j=0; j<i_j; j++){
-        for(int k=0; k<size_arr; k++){
-            if(arr[k][0] == i && arr[k][1] == j){
-                cout<<"*";
-                temp=true;
-                
-            }
-          
-        }
+fill_with_Value(x_arr,0,grid_size);
 
-         if(!temp) cout<<" ";
-           temp=false;
-        
-    }
-    cout<<endl;
+for(int i=0; i<size_arr; i++){
+    x_arr[(arr[i][0]*10)+arr[i][1]]=1;
 }
+cout<<endl;
 
-
-
+for(int i=0; i<grid_size; i++){
+    cout<<(x_arr[i]==0?"  ":"* ");
+    if((i+1)%10==0) cout<<endl;
+}
     return 0;
 }
