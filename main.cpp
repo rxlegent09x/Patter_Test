77f9b9c49b529332ac2ec0c62ef9c1ec26171fe4
{LEGENT_09X} <115865060+rxlegent09x@users.noreply.github.com>
2025-11-08 16:16:05 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ int main(){
 
 //can be edit the arr values to change the pattern
 
-    int arr[][2] = {{0,5},{0,4},{0,3},{0,2},{0,1},{0,0},{1,0},{2,0},{3,0},{3,1},{3,2},{3,3},{4,0},{5,0},{6,0},{7,0},{8,0},{6,2},{7,2},{8,2},{8,3},{8,4},{7,4},{6,4},{5,6},{6,6},{7,6},{8,6},{6,7},{5,8},{4,9},{7,8},{8,9}};
+    int arr[][2] = {{0,5},{0,4},{0,3},{0,2},{0,1},{0,0},{1,0},{2,0},{3,0},{4,0},{6,0},{5,0},{3,1},{3,2},{4,3},{5,3},{6,3},{6,4},{6,5},{5,5},{4,5},{4,7},{5,7},{6,7},{5,8},{4,9},{6,9},{2,7},{1,7},{0,7},{0,8},{0,9},{1,9},{2,9},{2,6}};
 
 int size_arr = (sizeof(arr)/sizeof(int))/2;
 int grid_size = 100;
@@ -22,16 +22,17 @@ int x_arr[grid_size];
 fill_with_Value(x_arr,0,grid_size);
 
 string zero="  ";
-string one = "* ";
+string one = " *";
 
 for(int i=0; i<size_arr; i++){
-    x_arr[(arr[i][0]*10)+arr[i][1]]=1;
+    x_arr[(arr[i][0]*(grid_size/10))+arr[i][1]]=1;
 }
 cout<<endl;
 
 for(int i=0; i<grid_size; i++){
     cout<<(x_arr[i]==0?zero:one);
-    if((i+1)%10==0) cout<<endl;
+    
+    if((i+1)%(grid_size/10)==0) cout<<endl;
 }
     return 0;
 }
